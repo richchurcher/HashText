e474b59c6c2c7cc9c7b4f55a0d2167ffdf7cc40f
Rich Churcher <rich.churcher@gmail.com>
2015-06-15 14:19:09 +1200

    Trial Xamarin project

---
+++ HashText/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace HashText
+{
+	[Activity (Label = "HashText", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Main);
+
+			EditText input = FindViewById<EditText> (Resource.Id.textToHash);
+			Button button = FindViewById<Button> (Resource.Id.SHAButton);
+			TextView hash = FindViewById <TextView> (Resource.Id.HashText);
+			
+			button.Click += delegate {
+				hash.Text = Hasher.HashString(input.Text);
+			};
+		}
+	}
+}
+
+
---
+++ HashText/WordHasher.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+using System;
+
+namespace HashText
+{
+	public class Hasher
+	{
+		public static string HashString(string raw)
+		{
+			if (string.IsNullOrWhiteSpace (raw)) {
+				return "";
+			}
+
+			return getSha256(raw);
+		}
+
+		public static string getSha256(string text)
+		{
+			SHA256Managed hashstring = new SHA256Managed();
+			byte[] hash = hashstring.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+			string hashString = "";
+			foreach (byte x in hash)
+			{
+				// "x2": hex
+				hashString += String.Format("{0:x2}", x);
+			}
+			return hashString;
+		}
+	}
+}
+
